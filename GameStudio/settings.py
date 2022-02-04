5e5770e3c43097ea2ef7310f933109591c15f5ea
Prachi_Jain <prachi.programmer@gmail.com>
2022-02-04 20:20:05 +0530

    modified settings.pyS

--- GameStudio/settings.py
+++ GameStudio/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS=[
@@ -135,6 +136,7 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
